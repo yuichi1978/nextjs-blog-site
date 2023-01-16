b8c93d81e471e1d2052abad7385389fffe8c9ac9
YuichiOta <rb19780519@gmail.com>
2023-01-16 15:58:22 +0900

    profile changed grid cssの変更

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyle.headingMd}>
-        <p>私はNextjsエンジニアです。<br />好きなフレームワークですがNext.jsです。</p>
+        <p>私はNext.jsエンジニアです。<br />好きなフレームワークはNext.jsです。</p>
       </section>
 
       <section>
